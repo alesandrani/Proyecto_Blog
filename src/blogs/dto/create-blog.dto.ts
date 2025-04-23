7c36822ba822cd6979874371a46668c1bac4ac3c
Aleksandra <asmorodina@ttypsa.es>
2025-04-23 10:04:06 +0200

    hecho

--- src/blogs/dto/create-blog.dto.ts
+++ src/blogs/dto/create-blog.dto.ts
@@ -1,1 +1,18 @@
-export class CreateBlogDto {}
+import { IsString, IsArray, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateBlogDto {
+  @ApiProperty()
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @IsString()
+  content: string;
+
+  @ApiProperty({ type: [String], required: false })
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  tags: string[];
+}
